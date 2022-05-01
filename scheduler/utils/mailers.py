bb34d45442c2913b167d1b4aafa7b6a05fb2089c
AlexGDC <alexandre.guyon@apsys-airbus.com>
2022-05-01 12:18:19 +0200

    0.9.8: wednesday message tests

--- scheduler/utils/mailers.py
+++ scheduler/utils/mailers.py
@@ -65,7 +65,46 @@ def cyberpostit():
 
 def wednesday():
     """ Wednesday recap of everything coming in the week """
-    pass
+    from scheduler.models.profile import Profile
+    profiles = Profile.objects.filter(mail_wednesday=True)
+    d1 = date.today()
+    d2 = date.today() + timedelta(days=6)
+    for p in profiles:
+        has_played, played_data = p.played_the(d1, d2)
+        has_masterized, masterized_data = p.masterized_the(d1, d2)
+        if has_played or has_masterized:
+            subject = f"[eXtraventures] Message du Mercredi !"
+            body = EmailBody()
+            body.stack(f"Salutations {p.nickname}!")
+            body.stack("")
+            body.stack("Vous recevez ce message car le flag 'Message du mercredi' est activé sur votre compte eXtraventures.")
+            body.stack(
+                "Vous ne recevrez ce message que si vous participez à des parties aujourd'hui. Si ce n'est pas le cas, pas de message!")
+            body.stack("¤ ¤ ¤")
+            body.stack("    Alors, puisque nous en somme là, c'est qu'il y a quelque chose à dire...")
+            if has_played:
+                body.stack("")
+                body.stack(f"    (a) Parties jouées:")
+                for s in played_data:
+                    body.stack("")
+                    body.stack(
+                        f"    - {s.title} par {s.mj.nickname}, jeu=[{s.game.name}], le {s.date_start.strftime(FMT_DATE_PRETTY)} à [{s.place}] (inscription ok)")
+                    body.stack(f"     Description:  {s.description} ")
+                    body.stack("")
+            if has_masterized:
+                body.stack("")
+                body.stack(f"    (b) Parties menées:")
+                for s in masterized_data:
+                    body.stack("")
+                    body.stack(
+                        f"    - [{s.title}] par {s.mj.nickname}, le {s.date_start.strftime(FMT_DATE_PRETTY)} à [{s.place}] (c'est toi le MJ!)")
+                    body.stack("")
+            body.stack("¤ ¤ ¤")
+            body.stack(f"::mouhahahahahahaha::\n\nVotre dévoué serviteur eXtraordinaire,\nFernando Casabuentes.")
+            sender = f'fernando.casabuentes@gmail.com'
+            targets = [p.user.email]
+            send_mail(subject, body.deliver(), sender, targets, fail_silently=False)
+
 
 def sunday():
     """ Sunday recap of actions to be done """
