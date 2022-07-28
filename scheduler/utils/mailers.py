97d86fc30094b55795ebcf1037b2aad3fad075fe
AlexGDC <alexandre.guyon@apsys-airbus.com>
2022-07-28 02:14:46 +0200

    Template 26

--- scheduler/utils/mailers.py
+++ scheduler/utils/mailers.py
@@ -30,9 +30,9 @@ def cyberpostit():
     profiles = Profile.objects.filter(mail_cyber_postit=True).filter(nickname="Zaffarelli")
     d1 = date.today()
     d2 = date.today() + timedelta(days=0)
-    d7 = date.today() + timedelta(days=7)
+    d7 = date.today() + timedelta(days=14)
     for p in profiles:
-        print(f"Handling... {p.nickname}")
+        # print(f"Handling... {p.nickname}")
         has_played, played_data = p.played_the(d1, d2)
         has_masterized, masterized_data = p.masterized_the(d1, d2)
         has_wanted, wanted_data = p.wanted_the(d1, d7)
@@ -79,11 +79,12 @@ def cyberpostit():
             sender = f'fernando.casabuentes@gmail.com'
             targets = [p.user.email]
             html_message = render_to_string('scheduler/emails/cyber_postit.html', context=email_data)
-            print(email_data)
-            print(html_message)
+            # print(email_data)
+            # print(html_message)
             plain_message = strip_tags(html_message)
             mail.send_mail(subject, plain_message, f"From <{sender}>", targets, html_message=html_message)
-
+        else:
+            print("Nothing to say !!")
 
 def wednesday():
     """ Wednesday recap of everything coming in the week """
