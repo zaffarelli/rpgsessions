78bfd637cd4031c0286312f58bcc08e877f6f6a2
AlexGDC <alexandre.guyon@apsys-airbus.com>
2022-11-27 23:19:38 +0100

    Profile form fix

--- scheduler/forms/profile_form.py
+++ scheduler/forms/profile_form.py
@@ -6,4 +6,5 @@ class ProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
         fields = "__all__"
+        exclude = ["face_style", "hair_style", "mouth_style"]
         hidden = ["id", "padid"]
